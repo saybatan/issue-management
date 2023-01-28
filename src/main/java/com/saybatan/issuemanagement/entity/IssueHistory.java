e0187bcf584de83a026bfedf274db47c711e16d0
Semih Aybatan <semihaybatan@gmail.com>
2023-01-28 21:41:25 +0300

    Project and IssueHistory added

--- src/main/java/com/saybatan/issuemanagement/entity/IssueHistory.java
+++ src/main/java/com/saybatan/issuemanagement/entity/IssueHistory.java
@@ -1,4 +1,26 @@
 package com.saybatan.issuemanagement.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "issue_history")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class IssueHistory extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @JoinColumn(name = "issue_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Issue issue;
+    @JoinColumn(name = "assignee_user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User assignee;
+
 }
--- src/main/java/com/saybatan/issuemanagement/entity/Project.java
+++ src/main/java/com/saybatan/issuemanagement/entity/Project.java
@@ -1,4 +1,24 @@
 package com.saybatan.issuemanagement.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "project")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Project extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "project_code", unique = true)
+    private String projectCode;
+    @Column(name = "project_name")
+    private String projectName;
+
 }
