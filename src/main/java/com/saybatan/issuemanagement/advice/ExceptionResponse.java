e666843979f98e2a0b761616a38ba0e5c0228345
Semih Aybatan <semihaybatan@gmail.com>
2023-02-04 23:46:34 +0300

    ExceptionHandling added

---
+++ src/main/java/com/saybatan/issuemanagement/advice/ExceptionResponse.java
@@ -0,0 +1,15 @@
+package com.saybatan.issuemanagement.advice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse {
+    private Date date;
+    private String message;
+}
---
+++ src/main/java/com/saybatan/issuemanagement/advice/IMExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.saybatan.issuemanagement.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class IMExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<?> handleExceptions(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.EXPECTATION_FAILED);
+    }
+}
