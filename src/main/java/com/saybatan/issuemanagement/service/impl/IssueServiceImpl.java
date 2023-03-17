adb875a27254af515a31bf71b69e4001a22db593
Semih Aybatan <semihaybatan@gmail.com>
2023-03-17 23:27:33 +0300

    IssueServiceImpl methods added

--- src/main/java/com/saybatan/issuemanagement/service/impl/IssueServiceImpl.java
+++ src/main/java/com/saybatan/issuemanagement/service/impl/IssueServiceImpl.java
@@ -2,7 +2,12 @@ package com.saybatan.issuemanagement.service.impl;
 
 import com.saybatan.issuemanagement.dto.IssueDTO;
 import com.saybatan.issuemanagement.entity.Issue;
+import com.saybatan.issuemanagement.entity.IssueStatus;
+import com.saybatan.issuemanagement.entity.Project;
+import com.saybatan.issuemanagement.entity.User;
 import com.saybatan.issuemanagement.repository.IssueRepository;
+import com.saybatan.issuemanagement.repository.ProjectRepository;
+import com.saybatan.issuemanagement.repository.UserRepository;
 import com.saybatan.issuemanagement.service.IssueService;
 import com.saybatan.issuemanagement.util.TPage;
 import org.modelmapper.ModelMapper;
@@ -11,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class IssueServiceImpl implements IssueService {
@@ -21,10 +27,14 @@ public class IssueServiceImpl implements IssueService {
 
     private final IssueRepository issueRepository;
     private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
 
-    public IssueServiceImpl(IssueRepository issueRepository, ModelMapper modelMapper) {
+    public IssueServiceImpl(IssueRepository issueRepository, ModelMapper modelMapper, UserRepository userRepository, ProjectRepository projectRepository) {
         this.issueRepository = issueRepository;
         this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+        this.projectRepository = projectRepository;
     }
 
     @Override
@@ -45,6 +55,11 @@ public class IssueServiceImpl implements IssueService {
         return modelMapper.map(issue, IssueDTO.class);
     }
 
+    public List<IssueDTO> getAll() {
+        List<Issue> issueList = issueRepository.findAll();
+        return Arrays.asList(modelMapper.map(issueList,IssueDTO[].class));
+    }
+
     @Override
     public TPage<IssueDTO> getAllPageable(Pageable pageable) {
 
@@ -57,16 +72,28 @@ public class IssueServiceImpl implements IssueService {
     }
 
     @Override
-    public Boolean delete(IssueDTO IssueDTO) {
-        return null;
+    public Boolean delete(IssueDTO issueDTO) {
+        Issue issue = modelMapper.map(issueDTO, Issue.class);
+        issueRepository.delete(issue);
+        return true;
     }
 
     @Override
     public IssueDTO update(Long id, IssueDTO issueDTO) {
-        return null;
+        Issue issue = issueRepository.getOne(id);
+        User user = userRepository.getOne(issueDTO.getAssignee().getId());
+        issue.setAssignee(user);
+        issue.setDate(issueDTO.getDate());
+        issue.setDescription(issueDTO.getDescription());
+        issue.setIssueStatus(issueDTO.getIssueStatus());
+        issue.setDetails(issueDTO.getDetails());
+        Project project = projectRepository.getOne(issueDTO.getProject().getId());
+        issue.setProject(project);
+        issueRepository.save(issue);
+        return modelMapper.map(issue,IssueDTO.class);
     }
 
-    public Boolean deleteById(Long id){
+    public Boolean deleteById(Long id) {
         issueRepository.deleteById(id);
         return true;
     }
