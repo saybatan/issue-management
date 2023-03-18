c189ed6211b74dde590beb23b866ca115d673915
Semih Aybatan <semihaybatan@gmail.com>
2023-03-19 01:31:31 +0300

    UserDTO fields added

--- src/main/java/com/saybatan/issuemanagement/dto/UserDTO.java
+++ src/main/java/com/saybatan/issuemanagement/dto/UserDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,4 +13,7 @@ public class UserDTO {
 
     private Long id;
     private String nameSurname;
+    private String username;
+    private String email;
+    private List<Long> issueIds;
 }
